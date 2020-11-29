129abd4c653066bf3bef6ad4ccac66f5e0064c18
kongo97 <s.costantini@presstoday.com>
2020-11-29 16:32:09 +0100

    Fix route home

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StreamController;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
